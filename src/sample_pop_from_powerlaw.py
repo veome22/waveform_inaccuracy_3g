b28d4c232aad47e39e533bb640bf24a44cf45b01
vkapil1 <vkapil1@login02.rockfish.cluster>
2023-01-20 13:28:46 -0500

    wrote initial script to simulate powerlaw population with single exponent, uniform q, uniform z in [0.02, 50.0], and all angles=pi/3

--- src/sample_pop_from_powerlaw.py
+++ src/sample_pop_from_powerlaw.py
@@ -2,13 +2,17 @@ import numpy as np
 import argparse
 from gwbench import injections
 from gwbench import network
-
+from astropy.cosmology import Planck18, z_at_value
+import astropy.units as u
+from gwbench import basic_relations as br
+from scipy import interpolate, integrate
+import dill
 import sys
 from mpi4py import MPI
 import os
 import time
 
-def get_network_response(inj_params, network_spec = ['CE2-40-CBO_C', 'CE2-20-CBO_S', 'ET_ET1', 'ET_ET2', 'ET_ET3'], approximant='IMRPhenomXAS', deriv_symbs_string = 'Mc eta DL tc phic iota ra dec psi'):
+def get_network_response(inj_params, f_max=1024., network_spec = ['CE2-40-CBO_C', 'CE2-20-CBO_S', 'ET_ET1', 'ET_ET2', 'ET_ET3'], approximant='IMRPhenomXAS', deriv_symbs_string = 'Mc eta DL tc phic iota ra dec psi'):
     
     # initialize the network with the desired detectors
     net = network.Network(network_spec)
@@ -20,7 +24,7 @@ def get_network_response(inj_params, network_spec = ['CE2-40-CBO_C', 'CE2-20-CBO
 
     # pick the desired frequency range
     f_min = 5.
-    f_max = 1024.
+    #f_max = 1024.
     d_f = 2**-4
     f = np.arange(f_min, f_max, d_f)
 
@@ -59,46 +63,81 @@ def get_network_response(inj_params, network_spec = ['CE2-40-CBO_C', 'CE2-20-CBO
 
     return net
 
+def p_m1(m1, alpha, m_min, m_max):
+    '''
+    BBH merger primary mass PDF.
+    '''
+    if alpha != -1:
+        N1 = 1 / ((m_max**(alpha+1) - m_min**(alpha+1))/(alpha+1))
+    else:
+        N1 = 1/(np.log(m_max/m_min))
+
+    return np.piecewise(m1, [(m1 < m_min), (m1 >= m_min)*(m1 < m_max), (m1 >= m_max)],
+                        [0, lambda m1: N1*m1**alpha, 0])
 
 
+parser = argparse.ArgumentParser(description='Generate a list of binaries sampled from a power law in m1, and uniform in q.')
 
-parser = argparse.ArgumentParser(description='Generate a list of binaries sampled from a power-law distribution.')
+parser.add_argument('-N', default="48", type=int,  help='number of merger events to sample (default: 48)')
+parser.add_argument('-o', '--outputdir',  default="../data/powerlaw_3.5", type=str,  help='directory of output networks (default: ../data/powerlaw_3.5)')
 
-parser.add_argument('-N', default="10", type=int,  help='number of merger events to sample (default: 10)')
-parser.add_argument('-a', '--alpha', default="-1.5", type=float, help='first exponent (default: -1.0)')
-#parser.add_argument('-b', '--beta', default="-2.0",  type=float, help='second exponent (default: -2.0)')
-parser.add_argument('--mmin', default="1.0",  type=float, help='minimum mass in Solar Masses (default: 1.0)')
-#parser.add_argument('--mt', default="20.0",  type=float, help= "mass of transition point in Solar Masses (default: 20.0)")
+parser.add_argument('--mmin', default="5.0",  type=float, help='minimum mass in Solar Masses (default: 5.0)')
 parser.add_argument('--mmax', default="100.0",  type=float, help='maximum mass in Solar Mass (default: 100.0)')
+parser.add_argument('--alpha', default="-3.5",  type=float, help='power law exponent for m1 distribution (default: -3.5 bsaed on LIGO GWTC3 PP model)')
+
+
+parser.add_argument('--qmin', default="0.01",  type=float, help='minimum mass ratio (q) (default: 0.01)')
+parser.add_argument('--qmax', default="0.99",  type=float, help='maximum mass ratio (q) (default: 0.99)')
+
 parser.add_argument('--offset', default="0",  type=int, help='starting index offset')
 
 args = vars(parser.parse_args())
 # print(args)
 
-N_events = args["N"]
-alpha = args["alpha"]
-#beta = args["beta"]
+num_injs = args["N"]
+output_path = args["outputdir"]
 m_min = args["mmin"]
-#m_t = args["mt"]
 m_max = args["mmax"]
+alpha = args["alpha"]
+q_min = args["qmin"]
+q_max = args["qmax"]
+
 offset = args["offset"]
    
-mass_dict = {'dist': 'power', 'alpha': alpha, 'mmin': m_min, 'mmax':m_max}
 seed=42
-num_injs = N_events
 
-spin_dict = {'geom': 'spherical', 'dim':3, 'chi_lo':0., 'chi_hi':1.}
-cosmo_dict = {'sampler': 'uniform_comoving_volume_rejection', 'zmin':0., 'zmax':3.}
-data = injections.injections_CBC_params_redshift(cosmo_dict,mass_dict,spin_dict,num_injs=num_injs,seed=seed, redshifted=0)
 
-Mcs, etas, chi1x, chi1y, chi1z, chi2x, chi2y, chi2z, iotas, ras, decs, psis, zs, DLs = data
+# sample redshifts uniormly in [0.02, 50] based on Bohranian & Sathyaprakash (2022)
+redshifts = np.random.uniform(0.02, 50, num_injs) 
+DLs = cosmo.luminosity_distance(redshifts).value
+
+# sample mass 1 from power law pdf
+m1_range = np.linspace(m_min, m_max, 1000)
+
+pdf_m1 = p_m1(m1, alpha, m_min, m_max)
+cdf_m1 = integrate.cumulative_trapezoid(pdf_m1, m1, initial=0)
+inv_cdf_m1 = interpolate.interp1d(cdf_m1 / cdf_m1[-1], m1)
+
+mass1 = inv_cdf_m1(np.random.rand(num_injs))
+
+# sample q uniformly between q_min and q_max
+q = np.random.uniform(q_min, q_max, num_injs)
+mass2 = mass1 * q
+    
+Mcs = (mass1*mass2)**(3/5) / (mass1+mass2)**(1/5) 
+etas = (mass1*mass2) / (mass1+mass2)**2
+
+# Convert source frame masses to detector frame masses
+Mcs = Mcs * (1+redshifts)
+mtotals = (mass1+mass2) * (1+redshifts)
+
+
+
+f_highs = np.round(4*br.f_isco_Msolar(mtotals))
+
 
 deriv_symbs_string = 'Mc eta DL tc phic iota ra dec psi'
 param_list = deriv_symbs_string.split()
-
-
-output_path = f'../data/powerlaw_{alpha}_networks'
-
 
 
 if not os.path.exists(output_path):
@@ -114,7 +153,7 @@ if __name__ == "__main__":
     for i, task in enumerate(range(num_injs)):
         
         if i%size!=rank: continue
-
+        
         inj_params = {
             'Mc':    Mcs[i],
             'eta':   etas[i],
@@ -127,20 +166,29 @@ if __name__ == "__main__":
             'DL':    DLs[i],
             'tc':    0,
             'phic':  0,
-            'iota':  iotas[i],
-            'ra':    ras[i],
-            'dec':   decs[i],
-            'psi':   psis[i],
+            'iota':  np.pi/3,
+            'ra':    np.pi/3,
+            'dec':   np.pi/3,
+            'psi':   np.pi/3,
             'gmst0': 0
-            }
+            } 
 
         sys.stdout.write("Event number %d (%d) being simulated by processor %d of %d\n" % (i, task, rank, size))
 
-        net1 = get_network_response(inj_params=inj_params, approximant='IMRPhenomXAS')
-        net2 = get_network_response(inj_params=inj_params, approximant='IMRPhenomD')
+        sys.stdout.write(f"Mc: {Mcs[i]:.2f}, eta: {etas[i]:.2f}")
+        
+        net2 = get_network_response(inj_params=inj_params, f_max=f_highs[i], approximant='IMRPhenomD')
 
-        net1.save_network(f'{output_path}/{offset+i}_xas_net')
-        net2.save_network(f'{output_path}/{offset+i}_d_net')    
+        if net2.cov is None:
+            sys.stdout.write(f"Matrix not invertible for Mc={Mcs[i]:.2f}, eta={etas[i]:.2f}, writing empty file\n.")
+            with open(f'{output_path}/{offset+i}_xas_net', "wb") as fi:
+                dill.dump(None, fi)
+            with open(f'{output_path}/{offset+i}_d_net', "wb") as fi:
+                dill.dump(None, fi)
+        else:        
+            net1 = get_network_response(inj_params=inj_params, f_max=f_highs[i], approximant='IMRPhenomXAS')
+            net1.save_network(f'{output_path}/{offset+i}_xas_net')
+            net2.save_network(f'{output_path}/{offset+i}_d_net')    
     
     end = time.time()
 
