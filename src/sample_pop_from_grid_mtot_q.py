implemented fixed SNR based grid computation

src/sample_pop_from_grid_mtot_q.py
@@ -12,7 +12,7 @@ from mpi4py import MPI
 import os
 import time
 
-def get_network_response(inj_params, f_max=1024., network_spec = ['CE2-40-CBO_C', 'CE2-20-CBO_S', 'ET_ET1', 'ET_ET2', 'ET_ET3'], approximant='IMRPhenomXAS', deriv_symbs_string = 'Mc eta DL tc phic iota ra dec psi'):
+def get_network_response(inj_params, f_max=1024., network_spec = ['CE2-40-CBO_C', 'CE2-20-CBO_S', 'ET_ET1', 'ET_ET2', 'ET_ET3'], approximant='IMRPhenomXAS', deriv_symbs_string = 'Mc eta DL tc phic iota ra dec psi', cond_num=1e25):
     
     # initialize the network with the desired detectors
     net = network.Network(network_spec)
@@ -56,16 +56,15 @@ def get_network_response(inj_params, f_max=1024., network_spec = ['CE2-40-CBO_C'
 
     # calculate the network and detector Fisher matrices, condition numbers,
     # covariance matrices, error estimates, and inversion errors
-    net.calc_errors()
+    net.calc_errors(cond_sup=cond_num)
 
     # calculate the 90%-credible sky area (in deg)
     net.calc_sky_area_90()
 
     return net
 
+def get_network_snr(inj_params, f_max=1024., network_spec = ['CE2-40-CBO_C', 'CE2-20-CBO_S', 'ET_ET1', 'ET_ET2', 'ET_ET3'], approximant='IMRPhenomXAS', deriv_symbs_string = 'Mc eta DL tc phic iota ra dec psi', cond_num=1e25):
 
-def get_network_snr(inj_params, network_spec = ['aLIGO_H','aLIGO_L','aLIGO_V'], approximant='IMRPhenomXAS', deriv_symbs_string = 'Mc eta DL tc phic iota ra dec psi', cond_num=1e25):
-    
     # initialize the network with the desired detectors
     net = network.Network(network_spec)
 
@@ -76,7 +75,7 @@ def get_network_snr(inj_params, network_spec = ['aLIGO_H','aLIGO_L','aLIGO_V'], 
 
     # pick the desired frequency range
     f_min = 5.
-    f_max = 512.
+    #f_max = 1024.
     d_f = 2**-4
     f = np.arange(f_min, f_max, d_f)
 
@@ -89,7 +88,7 @@ def get_network_snr(inj_params, network_spec = ['aLIGO_H','aLIGO_L','aLIGO_V'], 
         deriv_symbs_string=deriv_symbs_string,
         use_rot=use_rot
         )
-    
+
     # setup antenna patterns, location phase factors, and PSDs
     net.setup_ant_pat_lpf_psds()
 
@@ -205,7 +204,7 @@ if __name__ == "__main__":
         # Make sure the distance is set to achieve target SNR
         if target_snr is not None:
             # get the fiducial snr at DL
-            net1_snr = get_network_snr(inj_params=inj_params, network_spec=network_spec, approximant=approximant1)
+            net1_snr = get_network_snr(inj_params=inj_params, f_max=f_highs[i], approximant=approximant1)
             
             # calculate DL required to hit target_snr
             new_DL = DL * (net1_snr.snr / target_snr)