e1ec4d581a058bd02bb1d60909fa488d225643f4
vkapil1 <vkapil1@login02.rockfish.cluster>
2023-06-05 11:15:05 -0400

    initial commit for script to combine the results of fisher simulations

---
+++ src/combine_fisher_binaries.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import glob
+import argparse
+
+parser = argparse.ArgumentParser(description='Combine the output from fisher calculations.')
+
+parser.add_argument('-i', '--inputdir', default="../data/uniform_networks_f_max",  type=str, help='input directory of network files (default: ../data/powerlaw_smooth_hybrid_3G/hybr_0.0/')
+
+parser.add_argument('-o', '--outfile', default="../output/powerlaw_smooth_hybrid_3G/hybr_0.0.csv",  type=str, help='file to output the binary properties and biases (default: ../output/powerlaw_smooth_hybrid_3G/hybr_0.0.csv)')
+
+args = vars(parser.parse_args())
+
+inputdir = args["inputdir"]
+outfile = args["outfile"]
+
+files = glob.glob1(inputdir,f"hybr_*_bin_*")
+df = pd.DataFrame()
+
+for file in files:
+    try:
+        data = np.load(inputdir+file, allow_pickle=True)
+    except:
+        continue
+
+
+    df_inj = pd.DataFrame([data['inj_params'].item()])
+
+    param_cols = data['errs'].item().keys()
+    err_colnames = [str(param)+'_err' for param in list(param_cols)]
+    df_inj[err_colnames] = list(data['errs'].item().values())
+
+    bias_colnames = [str(param)+'_bias' for param in list(param_cols)]
+    df_inj[bias_colnames] = list(data['cv_bias'][0])
+
+    df_inj['snr'] = data['snr']
+    df_inj["index"] = data['index']
+
+    df_inj['faith'] = data['faith']
+    df_inj['inner_prod'] = data['inner_prod']
+    df_inj['z'] = data['z_inj']
+    df_inj['z_err'] = data['z_err']
+    df_inj['z_bias'] = data['z_bias']
+
+    df_inj.set_index("index", inplace=True)
+
+    df = pd.concat([df, df_inj], ignore_index=False)
+
+df.to_csv(outfile)
+
+        
