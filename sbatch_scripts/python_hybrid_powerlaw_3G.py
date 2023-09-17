cdc4919bd1869dd6b55b387728b2bbc319a5b1cf
vkapil1 <vkapil1@login02.rockfish.cluster>
2023-09-17 16:03:20 -0400

    updated fisher job script to use mpi correctly

--- sbatch_scripts/python_hybrid_powerlaw_3G.py
+++ sbatch_scripts/python_hybrid_powerlaw_3G.py
@@ -4,21 +4,25 @@ import os
 #offset = 100000
 #timeout = '10:00:00'
 
-N = 100000
+N = 100000 
 offset = 0
-timeout = '48:00:00'
+timeout = '24:00:00'
+nodes = 8
 
 hybrs = [0.0, 0.8, 0.825, 0.85, 0.875, 0.9, 0.925, 0.95, 0.96, 0.97, 0.98, 0.99, 1.0]
+#hybrs = [0.0]
 
 job_dir = '/home/vkapil1/scratch16-berti/3G_systematics_veome/sbatch_scripts/hybrid_powerlaw_3G_scripts/'
+output_dir = '../data/powerlaw_smooth_hybrid_3G'
 
 for hybr in hybrs:
     job_file = job_dir + f'hybr_{hybr}.script'
     script_text = f"""#!/bin/bash
 #SBATCH --job-name=hybrid_powerlaw_alpha_3.5_smooth_3G_hybr_{hybr}
 #SBATCH --time={timeout}
-#SBATCH --nodes=2
-#SBATCH --ntasks=96
+#SBATCH --nodes={nodes}
+#SBATCH --ntasks-per-node=48
+####SBATCH --ntasks=96
 #SBATCH --partition=defq
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=vkapil1@jh.edu
@@ -26,7 +30,10 @@ for hybr in hybrs:
 ml anaconda
 conda activate gwbench_dev
     
-mpirun -launcher fork -np 96 python ~/scratch16-berti/3G_systematics_veome/src/compute_hybrid_fisher.py -N {N} --offset {offset} -i \"../data/smooth_powerlaw_pop.npz\" -o \"../data/powerlaw_smooth_hybrid_3G\" --hybr {hybr} --approx1 \"IMRPhenomXAS\" --approx2 \"IMRPhenomD\"  --net_key \"3G\"
+echo $SLURM_JOB_NODELIST
+mpirun  -np {nodes*48} python ~/scratch16-berti/3G_systematics_veome/src/compute_hybrid_fisher.py -N {N} --offset {offset} -i \"../data/smooth_powerlaw_pop.npz\" -o \"../data/powerlaw_smooth_hybrid_3G_production\" --hybr {hybr} --approx1 \"IMRPhenomXAS\" --approx2 \"IMRPhenomD\"  --net_key \"3G\"
+
+###mpirun -launcher fork -np 96 python ~/scratch16-berti/3G_systematics_veome/src/compute_hybrid_fisher.py -N {N} --offset {offset} -i \"../data/smooth_powerlaw_pop.npz\" -o \"{output_dir}\" --hybr {hybr} --approx1 \"IMRPhenomXAS\" --approx2 \"IMRPhenomD\"  --net_key \"3G\"
     """
 
     with open(job_file, "w") as jf:
