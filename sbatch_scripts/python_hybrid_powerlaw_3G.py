490ddbf7bdbba908545fd23878f74763fd39f636
vkapil1 <vkapil1@login02.rockfish.cluster>
2023-09-18 06:48:16 -0400

    updated fisher script with modular behavior for input and output files

--- sbatch_scripts/python_hybrid_powerlaw_3G.py
+++ sbatch_scripts/python_hybrid_powerlaw_3G.py
@@ -1,19 +1,22 @@
 import os
 
-#N = 66
-#offset = 100000
-#timeout = '10:00:00'
+N = 259
+offset = 100000
+timeout = '8:00:00'
 
-N = 100000 
-offset = 0
-timeout = '24:00:00'
-nodes = 8
+#N = 100000 
+#offset = 0
+#timeout = '24:00:00'
+
+nodes = 5
 
 hybrs = [0.0, 0.8, 0.825, 0.85, 0.875, 0.9, 0.925, 0.95, 0.96, 0.97, 0.98, 0.99, 1.0]
-#hybrs = [0.0]
+
+pop_file = '../data/smooth_powerlaw_pop_extra.npz'
+#pop_file = '../data/smooth_powerlaw_pop.npz'
 
 job_dir = '/home/vkapil1/scratch16-berti/3G_systematics_veome/sbatch_scripts/hybrid_powerlaw_3G_scripts/'
-output_dir = '../data/powerlaw_smooth_hybrid_3G'
+output_dir = '../data/powerlaw_smooth_hybrid_3G_production'
 
 for hybr in hybrs:
     job_file = job_dir + f'hybr_{hybr}.script'
@@ -22,7 +25,6 @@ for hybr in hybrs:
 #SBATCH --time={timeout}
 #SBATCH --nodes={nodes}
 #SBATCH --ntasks-per-node=48
-####SBATCH --ntasks=96
 #SBATCH --partition=defq
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=vkapil1@jh.edu
@@ -31,9 +33,8 @@ ml anaconda
 conda activate gwbench_dev
     
 echo $SLURM_JOB_NODELIST
-mpirun  -np {nodes*48} python ~/scratch16-berti/3G_systematics_veome/src/compute_hybrid_fisher.py -N {N} --offset {offset} -i \"../data/smooth_powerlaw_pop.npz\" -o \"../data/powerlaw_smooth_hybrid_3G_production\" --hybr {hybr} --approx1 \"IMRPhenomXAS\" --approx2 \"IMRPhenomD\"  --net_key \"3G\"
+mpirun  -np {nodes*48} python ~/scratch16-berti/3G_systematics_veome/src/compute_hybrid_fisher.py -N {N} --offset {offset} -i \"{pop_file}\" -o \"{output_dir}\" --hybr {hybr} --approx1 \"IMRPhenomXAS\" --approx2 \"IMRPhenomD\"  --net_key \"3G\"
 
-###mpirun -launcher fork -np 96 python ~/scratch16-berti/3G_systematics_veome/src/compute_hybrid_fisher.py -N {N} --offset {offset} -i \"../data/smooth_powerlaw_pop.npz\" -o \"{output_dir}\" --hybr {hybr} --approx1 \"IMRPhenomXAS\" --approx2 \"IMRPhenomD\"  --net_key \"3G\"
     """
 
     with open(job_file, "w") as jf:
